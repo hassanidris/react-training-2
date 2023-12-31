25a01cb95a90db5e9e9d0ae2b7f390d396b209b1
Hassen Ahmed <hassan.creative@gmail.com>
2023-12-31 02:51:36 +0100

    building like and dislike component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 
-import { useState } from "react";
-import Alert from "./components/Alert";
+// import { BsFillCalendarFill } from "react-icons/bs";
 
-import Button from "./components/Button";
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+
+// import Button from "./components/Button";
+import Like from "./components/Like";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
-  const [alertVisible, setAlertVisibility] = useState(false);
+  // const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && (
+      <Like onClick={() => console.log("clicked")} />
+      {/* <Button onClick={() => console.log("clicked")}>My button</Button> */}
+      {/* <BsFillCalendarFill color="red" size="40" /> */}
+      {/* {alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
       )}
       <Button onClick={() => setAlertVisibility(true)}>Show Alert</Button>
 
       <div className="mb-5">
         <h2>Second section</h2>
-      </div>
+      </div> */}
 
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
     </div>
   );
 }
