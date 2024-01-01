655e3847c25ef4eb5973327744bb1d888fdfed47
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-01 23:37:50 +0100

    update state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "John",
-    },
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
+  // const [pizza, setPizza] = useState({
+  //   name: "Spaicy Pepperoni",
+  //   toppings: ["Mushroom"],
+  // });
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: "John",
+  //   },
+  // });
 
   const handleClick = () => {
-    setGame({ ...game, player: { ...game.player, name: "Hassan" } });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+    // Adding a toppings to the array
+    // setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    // setGame({ ...game, player: { ...game.player, name: "Hassan" } });
   };
 
   return (
     <div>
-      {game.player.name}
+      {/* {game.player.name} */}
       <button onClick={handleClick}>Update</button>
     </div>
   );
