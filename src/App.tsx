0b351c85dcdc56d96a906d2318564e1ab93e7ad3
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-01 20:47:51 +0100

    updating nested objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 94111,
+    },
   });
 
   const handleClick = () => {
-    // const newDrink = {
-    //   ...drink,
-    //   price: 6,
-    // };
-    setDrink({ ...drink, price: 6 }); // instead of creating new variable we can simplify it here
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 98734 },
+    });
   };
 
   return (
     <div>
-      {drink.price}
+      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
