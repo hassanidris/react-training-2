76676feb814d638c605be9369045937d08699755
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-01 20:38:56 +0100

    update objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import Message from "./Message";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    // const newDrink = {
+    //   ...drink,
+    //   price: 6,
+    // };
+    setDrink({ ...drink, price: 6 }); // instead of creating new variable we can simplify it here
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
