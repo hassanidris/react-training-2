4014a36efb41677daed9cab9cbfaee6eb71450a5
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-01 23:12:42 +0100

    Sharing State between Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, name: "Bug 1", fixed: false },
-    { id: 2, name: "Bug 2", fixed: false },
-  ]);
-
-  const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-  };
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.name} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <button onClick={handleClick}>Click Me</button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </div>
   );
 }
